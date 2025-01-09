a5d40c41df05e0fd602e9b9caffe48e19557f139
Adam Marshall <adam@appsynergy.net>
2025-01-09 11:07:16 +0000

    stub server, port switch

---
+++ src/server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+
+app.get("/express", (req, res) => {
+  res.send("Backend is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

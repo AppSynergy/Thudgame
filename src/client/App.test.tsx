09749bd19c567bc50f72ca4c8f2cb8df730a2cbf
Adam Marshall <adam@appsynergy.net>
2025-01-12 13:30:35 +0000

    ai test

--- src/client/App.test.tsx
+++ src/client/App.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import userEvent, { UserEvent } from "@testing-library/user-event";
 import App from "./App";
 import "@testing-library/jest-dom";
 import axios from "axios";
@@ -8,6 +8,12 @@ jest.mock("axios");
 
 const mockAxios = axios as jest.Mocked<typeof axios>;
 mockAxios.get.mockResolvedValue({ data: "I am mock get.", status: 200 });
+
+let user: UserEvent;
+
+beforeAll(() => {
+  user = userEvent.setup();
+});
 
 test("renders something", async () => {
   render(<App />);
@@ -18,17 +24,27 @@ test("renders something", async () => {
 
 test("can make a move and then reset the game", async () => {
   render(<App />);
-  const user = userEvent.setup();
 
   expect(screen.getByText(/Move number: 0/));
   await screen.findByText(/Your side is the Dwarfs/);
 
   user.click(screen.getAllByText("d")[0]);
-  user.click(screen.getByText(/a4/));
+  user.click(screen.getByText("a4"));
 
   await screen.findByText(/Trolls to move next/);
   expect(screen.getByText(/Move number: 1/));
 
-  await user.click(screen.getByText(/Play both sides./));
+  user.click(screen.getByText(/Play both sides./));
   await screen.findByText(/Move number: 0/);
+});
+
+test("can play against an ai opponent", async () => {
+  render(<App />);
+
+  user.click(screen.getByText(/Play the dwarfs vs. Slabhead/));
+  await screen.findByText(/You are playing against Slabhead/);
+
+  user.click(screen.getAllByText("d")[0]);
+  user.click(screen.getByText("a4"));
+  await screen.findByText(/Move number: 2/);
 });

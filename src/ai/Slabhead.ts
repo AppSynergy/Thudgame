c97f9ddc892c6b923461a267bcc17a8412260ee7
Adam Marshall <adam@appsynergy.net>
2025-02-23 09:57:30 +0000

    troll ai2

--- src/ai/Slabhead.ts
+++ src/ai/Slabhead.ts
@@ -1,6 +1,35 @@
 import { ThudAi } from "./";
-import { chooseRandom, filterMovesCapturable } from "../game/helper";
+import {
+  chooseRandom,
+  filterMovesCapturable,
+  getOtherSide,
+} from "../game/helper";
 import { Board, Move, Opt, Side, Square, TROLL } from "../game/types";
+import { radialSearch } from "../game/thud";
+import { rank, file } from "../game/Hex210";
+
+function findShortestAttackingMove(
+  side: Side,
+  board: Board,
+  moves: Opt<Move[]>
+): Opt<Move> {
+  let attackingMove = null as Opt<Move>;
+  let minDistance = Infinity;
+
+  moves?.map((move) => {
+    radialSearch(move.to, ({ to, distance }) => {
+      if (board[rank(to)][file(to)].piece == getOtherSide(side)) {
+        if (distance < minDistance) {
+          minDistance = distance;
+          attackingMove = move;
+        }
+      }
+      return false;
+    });
+  });
+
+  return attackingMove;
+}
 
 export default {
   name: "Slabhead",
@@ -10,10 +39,14 @@ export default {
   preferredSide: TROLL,
   playingSide: null,
 
-  decideMove: (_side: Side, _board: Board, moves: Opt<Move[]>): Opt<Move> => {
+  decideMove: (side: Side, board: Board, moves: Opt<Move[]>): Opt<Move> => {
     if (!moves) return null;
+
     const capturingMoves = filterMovesCapturable(moves);
     if (capturingMoves.length) return chooseRandom(capturingMoves) as Move;
+
+    const attackingMove = findShortestAttackingMove(side, board, moves);
+    if (attackingMove) return attackingMove;
 
     return chooseRandom(moves) as Move;
   },
